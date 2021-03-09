6be051d0061feb4c24706a80e5b29a25d7058a8b
Captaincake100 <79469448+Captaincake100@users.noreply.github.com>
2021-03-09 18:51:05 +0000

    v1

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask('app')
 
 @app.route('/')
 def hello_world():
   return render_template('#index.html', x = 'good')
 
-@app.route('/usr/<username>')
-def usr(username):
-  return render_template('user.html', usr = username)
+@app.route('/<usr>')
+def usr(usr):
+  return render_template('user.html', usr = usr)
   
+@app.route('/login', methods = ['POST', 'GET'])
+def login():
+  if request.method == 'POST':
+    user = request.form['nm']
+    return redirect(url_for('usr', usr=user))
+  else:
+    return render_template('login.html')
+
 app.run(host='0.0.0.0', port=8080)
